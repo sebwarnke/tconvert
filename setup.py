71d6d244d955a6ba1a21c817258fb73998dc05a9
Sebastian <sw-github@sebwarnke.com>
2020-12-25 23:53:04 +0100

    bump to version 1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tomorrow-pdf-converter',
-    version='0.1.1',
+    version='1.0.0',
     url='https://github.com/sebwarnke/tomorrow-bank-statement-converter',
     license='MIT',
     author='Sebastian Warnke',
